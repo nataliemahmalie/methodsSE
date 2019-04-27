1eac7d1d32b2da6413f846a285bc480120d0668c
shaharrubinshtein <37486628+shaharrubinshtein@users.noreply.github.com>
2019-04-27 20:54:17 +0300

    Update main.cpp
    shahar update

--- textBox/main.cpp
+++ textBox/main.cpp
@@ -11,14 +11,7 @@ int main()
     TextBox t(20,10);
     t.draw();
     auto handle = GetStdHandle(STD_INPUT_HANDLE);
-
-    //auto handleout = GetStdHandle(STD_OUTPUT_HANDLE);
-    //SetConsoleCursorPosition(handleout, {0,0});
-    //SetConsoleTextAttribute(handleout, (FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY) | (0 << 4));
-    //int x=0;
-    //int y=0;
     
-
 
         while(1)
         {
