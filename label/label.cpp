4c1c11095ddbad9617a2ce087623ad1dcef4d404
shaharrubinshtein <37486628+shaharrubinshtein@users.noreply.github.com>
2019-07-22 15:19:25 +0300

    label upload - still need fixes

---
+++ label/label.cpp
@@ -0,0 +1,25 @@
+#include "Label.h"
+#include <iostream>
+
+Label::Label(string value) : ControlBase(), value(value) {}; // problem with initialization list - to check and fix
+
+Label::Label(short height, short width, short top, short left, Color textColor, Color backgroundColor, Border border, string value) :
+     ControlBase(height, width, top, left, textColor, backgroundColor, border), value(value) {};
+
+string Label::getValue()
+{
+    return this->value;
+}
+
+void Label::setValue(string value)
+{
+    this->value = value;
+}
+
+
+void Label::draw(Graphics& g, int x, int y, size_t z)
+{
+    if (!z)
+        g.write(value);
+/////////////////////////////////////////////////
+}
