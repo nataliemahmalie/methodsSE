c53669c8bd87cac412adf18803094348b3a436a2
shaharrubinshtein <37486628+shaharrubinshtein@users.noreply.github.com>
2019-07-29 03:02:13 +0300

    Update Graphics.h

--- Graphics/Graphics.h
+++ Graphics/Graphics.h
@@ -5,28 +5,29 @@
 
 using namespace std;
 
-enum class Color { Black, Blue, Green, Red, Cyan, Purple, Orange, White };
+enum class Color { Black, Blue, Green, Red, Cyan, Purple, Orange, White, Grey };
 
 class Graphics
 {
-public:
-	Graphics(DWORD stdHandle = STD_OUTPUT_HANDLE);
-	void clearScreen();
-	void moveTo(int x, int y);
-	void setBackground(Color color);
-	void setForeground(Color color);
-	void write(string s);
-	void write(int x, int y, string s);
-	void write(wstring s);
-	void write(int x, int y, wstring s);
-	void setCursorVisibility(bool isVisible);
-	~Graphics();
+	public:
+		Graphics(DWORD stdHandle = STD_OUTPUT_HANDLE);
+		void clearScreen();
+		void moveTo(int x, int y);
+		void setBackgroundColor(Color color);
+		void setTextColor(Color color);
+		void write(string s);
+		void write(int x, int y, string s);
+		void write(wstring s);
+		void write(int x, int y, wstring s);
+		void setCursorVisibility(bool isVisible);
+		COORD GetCursorPosition();
+		Color getBackgroundColor() { return backgroundColor; }
+		Color getTextColor() { return textColor; }
 
-private:
-	HANDLE _console;
-	Color _background, _foreground;
-
-	void updateConsoleAttributes();
+	private:
+		HANDLE _console;
+		Color backgroundColor, textColor;
+		void updateConsoleAttributes();
 };
 
 bool isInside(int x, int y, int left, int top, int width, int height);
