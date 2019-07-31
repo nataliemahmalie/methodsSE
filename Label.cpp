3c63c042d3eafb8933e626eab908203e64729dba
nataliemahmalie <37626609+nataliemahmalie@users.noreply.github.com>
2019-07-31 22:35:42 +0300

    Add files via upload

--- Label.cpp
+++ Label.cpp
@@ -1,39 +1,47 @@
 #include "Label.h"
 #include <iostream>
 
-Label::Label() : Control(){ this->isFocused = false; }
 
-//label size is determined according to the text length
-Label::Label(string value) : Control(), value(value) {
+Label::Label() : Control(){ this->isFocus = false; }
+
+Label::Label(string value) : Control(), value(value) {//the text that it get and set,defualt size of the label by text we get
 	this->setWidth((short)(value.size()));
 	this->setHeight(1);
-	this->isFocused = false;
+	this->isFocus = false;
 };
 
-//adding background color and text color to the label
-Label::Label(string value, Color backgroundColor, Color textColor) : Control(), value(value) {setColor(backgroundColor, textColor);
+Label::Label(string value, Color background, Color foreground) : Control(), value(value) {//adding a background and frame color and set by the size
+	setColor(background, foreground);
 	this->setWidth((short)(value.size()));
 	this->setHeight(1);
-	this->isFocused = false;
+	this->isFocus = false;
 
 };
 
-Label::Label(string value, Color backgroundColor, Color textColor, short left, short top) : Control(left, top), value(value) {
-	setColor(backgroundColor, textColor);
+Label::Label(string value, Color background, Color foreground, short left, short top) : Control(left, top), value(value) {//inheratence from the interface control to now where to put the label
+	setColor(background, foreground);
 	this->setWidth((short)(value.size()));
 	this->setHeight(1);
-	this->isFocused = false;
+	this->isFocus = false;
+
 };
 
-//draw Label on screen
-void Label::draw(Graphics& g) { 
+
+string Label::getValue(){
+
+	return this->value;
+}
+
+void Label::setValue(string value){
+	this->setWidth((short)(value.size()));
+	this->setHeight(1);
+	this->value = value;
+}
+
+
+void Label::draw(Graphics& g) {//for default  draw the label 
 	drawIt(g, this);
 }
-void Label::drawInside(Graphics& g) {
+void Label::drawInside(Graphics& g) {//draw with parmeters
 	g.write(getLeft(), getTop(), this->value);
 }
-
-//default destructor
-Label::~Label(){
-
-};
--- Label.h
+++ Label.h
@@ -3,26 +3,23 @@
 #include <string>
 #include <iostream>
 
+
 using namespace std;
 
 class Label : public Control
 {
 private:
 	string value;
-	
 public:
 	Label();
 	Label(string value);
-	Label(string value, Color backgroundColor, Color textColor);
-	Label(string value, Color backgroundColor, Color textColor, short left, short top);
-	string getValue(){ return this->value;}
-	void setValue(string value){
-	this->setWidth((short)(value.size()));
-	this->setHeight(1);
-	this->value = value;}
-	void setTextColor(Color color) { this->textColor = color; }
-	void setBackgroundColor(Color color) { this->backgroundColor= color; }
+	Label(string value, Color background, Color foreground);
+	Label(string value, Color background, Color foreground, short left, short top);
+	string getValue();
+	void setValue(string value);
 	void draw(Graphics& g);
+	void setForeground(Color color) { this->foreground = color; }
+	void setBackground(Color color) { this->background= color; }
 	virtual void drawInside(Graphics& g);
-	~Label();
+
 };
